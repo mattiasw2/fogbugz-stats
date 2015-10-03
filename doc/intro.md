0cee27d669bd5bfc4137a8d243461c3802b61f72
Mattias W <mattiasw@gmail.com>
2015-10-03 11:40:11 +0200

    lein new app

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to fogbugz-stats
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/fogbugz_stats/core_test.clj
@@ -0,0 +1,7 @@
+(ns fogbugz-stats.core-test
+  (:require [clojure.test :refer :all]
+            [fogbugz-stats.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
