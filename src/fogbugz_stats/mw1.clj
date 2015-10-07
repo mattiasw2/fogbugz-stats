cc72c8729f4a43a386d87ca44869ce0b3ce46964
mattias <mattiasw@gmail.com>
2015-10-07 08:53:46 +0200

    Use defn2
    Works, except for recur

--- src/fogbugz_stats/mw1.clj
+++ src/fogbugz_stats/mw1.clj
@@ -19,13 +19,16 @@
   "Search and slurp for file in this dir, and all parents until found. Throw exception if not found. Max 50 levels"
   ([filename] (find-and-slurp filename 50 ""))
   ([filename level prefix]
+   ;; in order to allow :post
+   ;; (loop [filename filename level level prefix prefix]
    (if (< level 0) 
      (throw (Exception. (str "Not found: " filename)))
      (if (.exists (clojure.java.io/as-file (str prefix filename)))
        (slurp (str prefix filename))
        (recur filename (- level 1) (str "../" prefix))))))
 
-(defn of-xml-string
+
+(mwm/defn2 of-xml-string
   "Convert a UTF-8 string into an clojure structure"
   [str]
   (xml/parse (java.io.ByteArrayInputStream. (.getBytes str "UTF-8"))))
@@ -33,16 +36,16 @@
 ;; (def x1 (mw1/of-xml-string (:body @(http/get (:url Config)))))
 ;; (xml-keep-tag-content x1)
 
-(defn xml-keep-tag-content
+(mwm/defn2 xml-keep-tag-content
   "Keep recursive map and only keep :tag and :content as key and value.
    The resulting structure looks like json."
   ([pxml]
-   {:pre [(some? pxml)] :post [(some? %)]}
-   (if (:tag pxml)
+   ;; (if (:tag pxml)
+   ;; we want nil since (if
+   (if (:tag? pxml)
      (xml-keep-tag-content (:tag pxml)(:content pxml))
      pxml))
   ([tag content]
-   {:pre [(some? tag)(some? content)] :post [(some? %)]}
    {tag
     (if (seq? content)
       (let [res (for [pxml content] (xml-keep-tag-content pxml))]
